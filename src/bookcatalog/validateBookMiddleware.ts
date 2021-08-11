db5391f9a4802b93a165fe4719aec06603afea74
kwasniew <kwasniewski.mateusz@gmail.com>
2021-08-11 12:41:10 +0200

    zod

--- src/bookcatalog/validateBookMiddleware.ts
+++ src/bookcatalog/validateBookMiddleware.ts
@@ -13,3 +13,23 @@ export function validate(req: Request, res: Response, next: NextFunction) {
   }
 
 }
+
+// parse don't validate
+// make impossible states impossible
+
+// type RequestStatus = {status: 'loading'} | {status: 'loaded', data: string} | {status: 'error', error: Error}; // 3 states
+//
+// type RequestStatus = {
+//   isLoading: boolean;
+//   isError: boolean;
+// }; // 4 state
+//
+// function handleRequestStatus(reqStatus: RequestStatus) {
+//   if(reqStatus.status === 'loaded') {
+//     console.log(reqStatus.data)
+//   } else if(reqStatus.status === 'error') {
+//     console.log(reqStatus.error);
+//   } else {
+//     // console.log(reqStatus.err)
+//   }
+// }
