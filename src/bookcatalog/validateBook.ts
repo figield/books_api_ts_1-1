c5922dcc76d5826108225318a95d73a551ae55c4
kwasniew <kwasniewski.mateusz@gmail.com>
2021-08-11 12:21:53 +0200

    parse dont validate

--- src/bookcatalog/validateBook.ts
+++ src/bookcatalog/validateBook.ts
@@ -1,22 +1,13 @@
-import Joi from "joi";
-import {BookDTO} from "./book";
+import { z } from "zod";
 
-const authorSchema = Joi.string().required().min(1).max(50);
-
-const schema = Joi.object({
-  title: Joi.string().trim().required().min(1).max(100),
-  authors: Joi.array().items(authorSchema).required().min(1).max(10),
-  description: Joi.string().allow("").max(5000),
-  isbn: Joi.string().required().uppercase().length(10),
+const BookSchema = z.object({
+    title: z.string().min(1).max(100),
+    authors: z.array(z.string().min(1).max(50)).min(1).max(10),
+    description: z.optional(z.string().max(5000)),
+    isbn: z.string().length(10)
 });
+export type BookDTO = z.infer<typeof BookSchema>;
 
-// parse, don't validate
 export function validateBook(book: unknown) {
-  const result = schema.validate(book, {
-    allowUnknown: false,
-    convert: true,
-    abortEarly: false,
-  });
-  return result.error ? result.error.details : null;
-  // return result.error ? Maybe.Just(result.error.details) : Maybe.None();
+    return BookSchema.safeParse(book);
 }
--- src/bookcatalog/validateBookMiddleware.ts
+++ src/bookcatalog/validateBookMiddleware.ts
@@ -3,14 +3,13 @@ import {Request, Response, NextFunction} from "express";
 import {StatusError} from "../error";
 
 export function validate(req: Request, res: Response, next: NextFunction) {
-  const validateErrors = validateBook(req.body);
+  const result = validateBook(req.body);
 
-  if (validateErrors) {
-    const error = new StatusError(validateErrors, 400);
-    // error.message = validateErrors;
-    // error.status = 400;
+  if(!result.success) {
+    const error = new StatusError(result.error, 400);
     next(error);
   } else {
     next();
   }
+
 }
