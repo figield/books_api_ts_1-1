1fcdd85ef311382e146bae6c985aef0b15583143
kwasniew <kwasniewski.mateusz@gmail.com>
2021-08-11 11:56:13 +0200

    errors

--- src/error.ts
+++ src/error.ts
@@ -1,15 +1,17 @@
-export function notFound(req, res, next) {
+import {RequestHandler, ErrorRequestHandler} from "express";
+
+export const notFound: RequestHandler = (req, res, next) => {
   const err = new Error("Not Found");
   // @ts-ignore
   err.status = 404;
   next(err);
-}
+};
 
-export function errorHandler(err, req, res, next) {
+export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   console.error(err.stack);
   res.status(err.status || 500);
   res.json({ message: err.message, error: isProduction() ? {} : err.stack });
-}
+};
 
 function isProduction() {
   return process.env.NODE_ENV === "production";
